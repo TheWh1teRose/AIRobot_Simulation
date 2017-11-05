632cafc86e36e1853f45318467b2213ba9957af8
W1teRose <felix.loos@t-online.de>
2017-11-05 17:33:34 +0100

    Add controls and IK to robot

---
+++ AIRobotSimulation/Assets/Scripts/ControlArm.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlArm : MonoBehaviour {
+
+    public float moveSpeed = 1;
+    public float roationSpeed = 1;
+    public float grapSpeed = 1;
+    public GameObject leftGrap;
+    public GameObject rightGrap;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime,
+            moveSpeed * -Input.GetAxis("Height") * Time.deltaTime,
+            moveSpeed * -Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Rotate(roationSpeed * Input.GetAxis("Rotation") * Time.deltaTime,
+            0f,
+            0f);
+
+        leftGrap.transform.Translate(moveSpeed * Input.GetAxis("Grap") * Time.deltaTime,
+            0f,0f);
+        rightGrap.transform.Translate(moveSpeed * -Input.GetAxis("Grap") * Time.deltaTime,
+            0f, 0f);
+    }
+}
