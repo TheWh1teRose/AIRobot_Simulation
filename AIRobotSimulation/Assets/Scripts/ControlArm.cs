4c484e9dcc3cf9951c7db2647b717145e9e70184
W1teRose <felix.loos@t-online.de>
2017-11-07 18:50:50 +0100

    Add a grabing objects
    Added the function to grab objects and fix some gavity issues.

--- AIRobotSimulation/Assets/Scripts/ControlArm.cs
+++ AIRobotSimulation/Assets/Scripts/ControlArm.cs
@@ -6,7 +6,6 @@ public class ControlArm : MonoBehaviour {
 
     public float moveSpeed = 1;
     public float roationSpeed = 1;
-    public float grapSpeed = 1;
     public GameObject leftGrap;
     public GameObject rightGrap;
 
@@ -18,15 +17,10 @@ public class ControlArm : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(moveSpeed * Input.GetAxis("Horizontal")*Time.deltaTime,
-            moveSpeed * -Input.GetAxis("Height") * Time.deltaTime,
-            moveSpeed * -Input.GetAxis("Vertical") * Time.deltaTime);
+            moveSpeed * -Input.GetAxis("Vertical") * Time.deltaTime,
+            moveSpeed * -Input.GetAxis("Height") * Time.deltaTime);
         transform.Rotate(roationSpeed * Input.GetAxis("Rotation") * Time.deltaTime,
             0f,
             0f);
-
-        leftGrap.transform.Translate(moveSpeed * Input.GetAxis("Grap") * Time.deltaTime,
-            0f,0f);
-        rightGrap.transform.Translate(moveSpeed * -Input.GetAxis("Grap") * Time.deltaTime,
-            0f, 0f);
     }
 }
---
+++ AIRobotSimulation/Assets/Scripts/CubeControler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControler : MonoBehaviour
+{
+
+    public GameObject stick;
+    private bool isSticking = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSticking)
+        {
+            transform.position = stick.transform.position;
+            transform.rotation = stick.transform.rotation;
+
+            if (Input.GetKey("b") && isSticking)
+            {
+                isSticking = false;
+                //Used to avoid a shot effect when releasing an object
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == stick)
+        {
+            if (Input.GetKey("g") && !isSticking)
+            {
+                isSticking = true;
+                //Used to avoid a shot effect when releasing an object
+                GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+    }
+}
