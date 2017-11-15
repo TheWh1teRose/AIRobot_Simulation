64edb734997daae8d230c7251777375e2042fbf0
Felix Surface <felix.loos@t-online.de>
2017-11-15 09:06:28 +0100

    Fix some issues and add an converter function for the controls
    Fixed the issue with the position matrix. Add an function that converts the controls to an onhot array thats needed for machine laerning.

--- ControlerScripts/dataGraper.py
+++ ControlerScripts/dataGraper.py
@@ -6,7 +6,7 @@ import threading
 import struct
 
 #stores the received control and position-matrix in a global variables
-controls = "0:0:0"
+controlsStr = "0:0:0"
 posMatrixStr = "\x00"
 
 #start the UDP connection to Unity for the controls and the position-matrix
@@ -18,8 +18,8 @@ def getControls():
 	#get the controls from the unity Input.GetAxis function
 	#in the format: [horizontal]:[vertical]:[height]:[graping]
 	while True:
-		global controls
-		controls, address = cntControls.recvData()
+		global controlsStr
+		controlsStr, address = cntControls.recvData()
 
 #define function to get the position-matrix from the Unity UDP sender 
 def getPosMatrix():
@@ -29,29 +29,43 @@ def getPosMatrix():
 		global posMatrixStr
 		posMatrixStr, address = cntPosMatrix.recvData()
 
+#define function to convert the controls to an oneHot Array; -1:1:0:1 -> 0:1:1:0:0:0:1:0 
+def convertControls(controls):
+	beforeControls = controlsStr.split(':')
+	oneHotControls = []
+	for i, value in enumerate(beforeControls):
+		if value >= 0:
+			oneHotControls.append(value)
+			oneHotControls.append(0)
+		else: 
+			oneHotControls.append(0)
+			oneHotControls.append(abs(value))
+	return np.array(oneHotControls) 
 
+#start the contollers
 cntControls.startController()
 cntPosMatrix.startController()
 print("start Controller")
 
-#start the getControls function in a new thread to avoid data stagnation while reading image data
+#start the getControls and getPosMAtrix function in a new thread to avoid data stagnation while reading image data
 threadControls = threading.Thread(target=getControls)
 threadPosMatrix = threading.Thread(target=getPosMatrix)
 threadControls.start()
 threadPosMatrix.start()
 
 while True:
-	#print controls to console
+	#convert controls to onehot and print it
+	controls = convertControls(controlsStr)
 	print(controls)
-	posMatrix = struct.unpack_from("f", "\x00\x00\x00\x00\x07\x80\x00\x03")
-	posMatrix = np.fromstring(posMatrixStr, dtype='float')
-	#if posMatrixStr != "\x00": posMatrix = np.reshape(posMatrix, (35,35,11))
+	#convert the bytes from the string in to an float32 numpy array
+	posMatrix = np.fromstring(posMatrixStr, np.float32)
+	#reshape the array to an 35,35,11 numpy array
+	if posMatrixStr != "\x00": posMatrix = np.reshape(posMatrix, (35,35,11))
 	print(posMatrix.shape)
 
 	#read the screen, put it in to an numpy array and show it in a window
 	printscreen = np.array(ImageGrab.grab(bbox=(5,45,755,545)))
 	cv2.imshow('screen', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
-
 
 
 	if cv2.waitKey(25) & 0xFF == ord('q'): #quit statement 
