ca141995a78df2bb0a5b7e4fdafaf2227df43dc6
Felix Surface <felix.loos@t-online.de>
2017-12-11 11:12:54 +0100

    t

--- Models/CNN_utils.py
+++ Models/CNN_utils.py
@@ -1,49 +1,59 @@
 import numpy as np
 import tensorflow as tf
 
-def create_weights(shape):
+def create_weights(shape, name=None):
 	return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
 
-def create_biases(lenght):
+def create_biases(lenght, name=None):
 	return tf.Variable(tf.constant(0.05, shape=[lenght]))
 
-def create_conv_layer(input, num_input_channels, filter_size, num_filters, cnn_stride=1):
-	shape = [filter_size, filter_size, num_input_channels, num_filters]
-	weights = create_weights(shape)
-	print("test")
-	print(weights)
-	biases = create_biases(num_filters)
-	print(input)
-	layer = tf.nn.conv2d(input=input, filter=weights, strides=[1, cnn_stride, cnn_stride, 1], padding='SAME')
-	layer += biases
+def create_conv_layer(input, num_input_channels, filter_size, num_filters, name=None, cnn_stride=1):
+	with tf.variable_scope(name) as scope:
+		shape = [filter_size, filter_size, num_input_channels, num_filters]
 
-	layer = tf.nn.relu(layer)
+		weights_name = 'weights ' + name
+		weights = create_weights(shape, weights_name)
 
-	return layer, weights
+		biases_name = 'biases ' + name
+		biases = create_biases(num_filters, biases_name)
 
-def create_fully_connected_layer(input, num_inputs, num_outputs, relu=True):
-	weights = create_weights([num_inputs, num_outputs])
-	biases = create_biases(num_outputs)
+		layer_name = 'conv2d ' + name
+		layer = tf.nn.conv2d(input=input, filter=weights, strides=[1, cnn_stride, cnn_stride, 1], padding='SAME', name=layer_name)
+		layer += biases
 
-	layer = tf.matmul(input, weights) + biases
+		relu_name = 'relu ' + name
+		layer = tf.nn.relu(layer; name=relu_name)
 
-	if relu:
-		layer = tf.nn.relu(layer)
+		return layer, weights
 
-	return layer
+def create_fully_connected_layer(input, num_inputs, num_outputs, relu=True, name=None):
+	with tf.variable_scope(name) as scope:
+		weights_name = 'weights ' + name
+		weights = create_weights([num_inputs, num_outputs], name=weights_name)
+		biases_name = 'biases ' + name
+		biases = create_biases(num_outputs, name=biases_name)
 
-def local_response_normalization(input):
-	return tf.nn.local_response_normalization(input)
+		layer = tf.matmul(input, weights) + biases
 
-def dropout(input, keep_prob=0.5):
-	return tf.nn.dropout(input, keep_prob)
+		if relu:
+			relu_name = 'relu ' + name
+			layer = tf.nn.relu(layer, name=relu_name)
 
-def pooling(input, pool_ksize=2, pool_stride=2):
-	return tf.nn.max_pool(input, [1, pool_ksize, pool_ksize, 1], [1, pool_stride, pool_stride, 1], padding='SAME')
+		return layer
 
-def flatten_layer(layer):
-	layer_shape = layer.get_shape()
-	num_features = layer_shape[1:4].num_elements()
-	layer_flat = tf.reshape(layer, [-1, num_features])
-	return layer_flat, num_features
+def local_response_normalization(input, name=None):
+	return tf.nn.local_response_normalization(input, name=name)
+
+def dropout(input, keep_prob=0.5, name=None):
+	return tf.nn.dropout(input, keep_prob, name=name)
+
+def pooling(input, pool_ksize=2, pool_stride=2, name=None):
+	return tf.nn.max_pool(input, [1, pool_ksize, pool_ksize, 1], [1, pool_stride, pool_stride, 1], padding='SAME', name=name)
+
+def flatten_layer(layer, name=None):
+	with tf.variable_scope(name) as scope:
+		layer_shape = layer.get_shape()
+		num_features = layer_shape[1:4].num_elements()
+		layer_flat = tf.reshape(layer, [-1, num_features])
+		return layer_flat, num_features
 
---
+++ Models/first_cnn_2.py
@@ -0,0 +1,220 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
+import glob
+import CNN_utils as cnn
+import pickle
+import time
+import datetime
+import functools
+
+path = '/home/paperspace/Desktop/AI/AIRobot_Simulation/Data/*.pickle'
+file = glob.glob(path)
+data = None
+print(file)
+
+for f in file:
+	if data is None:
+		print("loaded: " + f)
+		data = pickle.load(open(f, "rb"))
+		print("loaded: " + f)
+		X = data[0]
+		y = data[1]
+	else:
+		data = pickle.load(open(f, "rb"))
+		X = np.concatenate((X, data[0]))
+		y = np.concatenate((y, data[1]))
+
+
+X = X[:,:,:,:3]
+print(X.shape)
+
+x_min = X.min(axis=(1,2), keepdims=True)
+x_max = X.max(axis=(1,2), keepdims=True)
+X = (X - x_min)/(x_max - x_min)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+X_test, X_valid = np.array_split(X_test, 2)
+y_test, y_valid = np.array_split(y_test, 2)
+print("X train: " + str(X_train.shape))
+print("y train: " + str(y_train.shape))
+print("X test: " + str(X_test.shape))
+print("y test: " + str(y_test.shape))
+print("X valid: " + str(X_valid.shape))
+print("y valid: " + str(y_valid.shape))
+
+print("sample: ")
+print(X_train[5,:10,:10,1])
+print(y_train[5])
+
+def accuracy(predictions, labels):
+	return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) / predictions.shape[0])
+
+#architecture
+filter_size1 = 3
+num_filters1 = 64
+filter_size2 = 3
+num_filters2 = 64
+#maxpool
+filter_size3 = 3
+num_filters3 = 128
+filter_size4 = 3
+num_filters4 = 128
+#maxpool
+filter_size5 = 3
+num_filters5 = 256
+filter_size6 = 3
+num_filters6 = 256
+#maxpool
+filter_size7 = 3
+num_filters7 = 512
+filter_size8 = 3
+num_filters8 = 512
+#maxpool
+filter_size9 = 3
+num_filters9 = 512
+filter_size10 = 3
+num_filters10 = 512
+#maxpool
+fc_size1 	 = 4096
+fc_size2 	 = 4096
+fc_size3 	 = 1000
+
+image_width = 300
+image_height = 300
+image_depth = 3
+num_lable = 8
+
+
+epochs = 3000
+start_learning_rate = 0.0001
+batch_size = 16
+keep_probability = 0.5
+
+graph = tf.Graph()
+with graph.as_default():
+	tf_X_train = tf.placeholder(tf.float32, shape=[None, image_height, image_width, image_depth], name='X')
+	tf_y_train = tf.placeholder(tf.float32, shape=[None, num_lable], name='y')
+	tf_y_train_cls = tf.argmax(tf_y_train, dimension=1)
+	keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+	global_step = tf.Variable(0, trainable=False, name='global step')
+	learning_rate = tf.train.exponential_decay(start_learning_rate, global_step, 100, 0.96, staircase=True)
+	print(tf_X_train)
+
+	tf_images = tf.image.resize_images(tf_X_train, [224, 224], name='rezise image')
+
+	with tf.name_scope('first conv layer 64 filter') as scope:
+		layer_conv1, weights_conv1 = cnn.create_conv_layer(tf_images, image_depth, filter_size1, num_filters1, name='1 conv layer')
+		layer_conv2, weights_conv2 = cnn.create_conv_layer(layer_conv1, num_filters1, filter_size2, num_filters2, name='2 conv layer')
+		layer_conv2_pool = cnn.pooling(layer_conv2, name='layer 2 pooling')
+
+	with tf.name_scope('secound conv layer 128 filter') as scope:
+		layer_conv3, weights_conv3 = cnn.create_conv_layer(layer_conv2_pool, num_filters2, filter_size3, num_filters3, name='3 conv layer')
+		layer_conv4, weights_conv4 = cnn.create_conv_layer(layer_conv3, num_filters3, filter_size4, num_filters4, name='4 conv layer')
+		layer_conv4_pool = cnn.pooling(layer_conv3, name='layer 4 pooling')
+
+	with tf.name_scope('thirth conv layer 256 filter') as scope:
+		layer_conv5, weights_conv5 = cnn.create_conv_layer(layer_conv4_pool, num_filters4, filter_size5, num_filters5, name='5 conv layer')
+		layer_conv6, weights_conv6 = cnn.create_conv_layer(layer_conv5, num_filters5, filter_size6, num_filters6, name='6 conv layer')
+		layer_conv6_pool = cnn.pooling(layer_conv6, name='layer 6 pooling')
+
+	with tf.name_scope('fourth conv layer 512 filter') as scope:
+		layer_conv7, weights_conv7 = cnn.create_conv_layer(layer_conv6_pool, num_filters6, filter_size7, num_filters7, name='7 conv layer')
+		layer_conv8, weights_conv8 = cnn.create_conv_layer(layer_conv7, num_filters7, filter_size8, num_filters8, name='8 conv layer')
+		layer_conv8_pool = cnn.pooling(layer_conv8, name='layer 8 pooling')
+
+	with tf.name_scope('fivth conv layer 512 filter') as scope:
+		layer_conv9, weights_conv9 = cnn.create_conv_layer(layer_conv8_pool, num_filters8, filter_size9, num_filters9, name='9 conv layer')
+		layer_conv10, weights_conv10 = cnn.create_conv_layer(layer_conv9, num_filters9, filter_size10, num_filters10, name='10 conv layer')
+		layer_conv10_pool = cnn.pooling(layer_conv10, name='layer 10 pooling')
+
+	with tf.name_scope('fully connected layer') as scope:
+		layer_flat, num_features = cnn.flatten_layer(layer_conv10_pool, name='flatten layer')
+		fc_layer1 = cnn.create_fully_connected_layer(layer_flat, num_features, fc_size1, name='1 fully connected')
+		fc_layer1_dropout = cnn.dropout(fc_layer1, keep_prob, name='1 layer dropout')
+		fc_layer2 = cnn.create_fully_connected_layer(fc_layer1_dropout, fc_size1, fc_size2, name='2 fully connected')
+		fc_layer2_dropout = cnn.dropout(fc_layer2, keep_prob, name='2 layer dropout')
+		fc_layer3 = cnn.create_fully_connected_layer(fc_layer2_dropout, fc_size2, fc_size3, name='3 fully connected')
+		fc_layer4 = cnn.create_fully_connected_layer(fc_layer3, fc_size3, num_lable, relu=False, name='4 fully connected')
+
+
+	y_pred = tf.nn.softmax(fc_layer4)
+	y_pred_cls = tf.argmax(y_pred, dimension=1)
+	cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=fc_layer4, labels=tf_y_train)
+	cost = tf.reduce_mean(cross_entropy)
+	optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost, global_step=global_step)
+	correct_prediction = tf.equal(y_pred_cls, tf_y_train_cls)
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+	tf.summary.scalar('Accuracy', accuracy)
+	tf.summary.scalar('loss/cost', cost)
+	merged = tf.summary.merge_all()
+	saver = tf.train.Saver()
+
+
+tf.reset_default_graph()
+with tf.Session(graph=graph) as session:
+	tf.global_variables_initializer().run()
+	train_writer = tf.summary.FileWriter('statistics/train', session.graph)
+	total_epochs = 1
+	courent_batch_position = 0
+
+
+	for i in range(epochs):
+		if (courent_batch_position + batch_size) <= X_train.shape[0]:
+			nextBatchPosition = (courent_batch_position+batch_size)
+			X_batch = X_train[courent_batch_position : nextBatchPosition,...]
+			y_batch = y_train[courent_batch_position : nextBatchPosition,...]
+			courent_batch_position += batch_size
+		else:
+			overLapp = ((courent_batch_position + batch_size) - X_train.shape[0])
+			X_batch = X_train[: overLapp,...]
+			y_batch = y_train[: overLapp,...]
+			courent_batch_position = overLapp
+
+		session.run(optimizer, feed_dict={tf_X_train: X_batch, tf_y_train: y_batch, keep_prob: keep_probability})
+		summary, acc = session.run([merged, accuracy], feed_dict={tf_X_train: X_batch, tf_y_train: y_batch, keep_prob: 1})
+		train_writer.add_summary(summary, total_epochs)
+		total_epochs += 1
+		if total_epochs % 100 == 0:
+
+			all_acc_valid = []
+			courent_batch_position_valid = 0
+			for i in range(int(X_valid.shape[0]/batch_size)+1):
+				if (courent_batch_position_valid + batch_size) <= X_valid.shape[0]:
+					nextBatchPosition_valid = (courent_batch_position_valid+batch_size)
+					X_batch_valid = X_valid[courent_batch_position_valid : nextBatchPosition_valid,...]
+					y_batch_valid = y_valid[courent_batch_position_valid : nextBatchPosition_valid,...]
+					courent_batch_position_valid += batch_size
+				else:
+					overLapp_valid = ((courent_batch_position_valid + batch_size) - X_valid.shape[0])
+					X_batch_valid = X_valid[: overLapp_valid,...]
+					y_batch_valid = y_valid[: overLapp_valid,...]
+					courent_batch_position_valid = overLapp_valid
+
+				all_acc_valid.append(session.run(accuracy, feed_dict={tf_X_train: X_batch_valid, tf_y_train:y_batch_valid, keep_prob: 1}))
+			print("Valid Accuracy: " + str(functools.reduce(lambda x_, y_: x_ + y_, all_acc_valid)/len(all_acc_valid)))
+			print("loss: " + str())
+
+			print("epoch: {}; Train Accuracy: {}".format(total_epochs, acc))
+			print("batches seen: " + str(tf.train.global_step(session, global_step)))
+
+	save_path = saver.save(session, "model.ckpt")
+	all_acc_test = []
+	for i in range(int(X_test.shape[0]/batch_size)+1):
+		if (courent_batch_position + batch_size) <= X_test.shape[0]:
+			nextBatchPosition = (courent_batch_position+batch_size)
+			X_batch = X_test[courent_batch_position : nextBatchPosition,...]
+			y_batch = y_test[courent_batch_position : nextBatchPosition,...]
+			courent_batch_position += batch_size
+		else:
+			overLapp = ((courent_batch_position + batch_size) - X_test.shape[0])
+			X_batch = X_test[: overLapp,...]
+			y_batch = y_test[: overLapp,...]
+			courent_batch_position = overLapp
+
+	all_acc_test.append(session.run(accuracy, feed_dict={tf_X_train: X_batch, tf_y_train:y_batch, keep_prob: 1}))
+	print("Test Accuracy: " + str(functools.reduce(lambda x, y: x + y, all_acc_test)/len(all_acc_test)))
+	session.close()
